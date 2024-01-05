8fb7efbdbd910e1151f3fa3ca5d8458ac1dc078c
mylxsw <mylxsw@aicode.cc>
2024-01-05 14:22:27 +0800

    增加用户评价聊天结果好、不好的支持

---
+++ migrate/data/20240105_ddl.go
@@ -0,0 +1,14 @@
+package migrate
+
+import "github.com/mylxsw/eloquent/migrate"
+
+func Migrate20240105DDL(m *migrate.Manager) {
+	m.Schema("20240105-ddl").Table("chat_messages", func(builder *migrate.Builder) {
+		builder.Integer("first_letter_cost", false, true).Nullable(true).Comment("第一个字符响应耗时，单位微秒")
+		builder.Integer("total_cost", false, true).Nullable(true).Comment("总耗时，单位微秒")
+	})
+
+	m.Schema("20240105-ddl").Table("chat_group_message", func(builder *migrate.Builder) {
+		builder.Integer("total_cost", false, true).Nullable(true).Comment("总耗时，单位微秒")
+	})
+}
